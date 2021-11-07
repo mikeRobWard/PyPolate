22d9ab986edb53d9f932b08a8322ea6356b5d630
Michael Ward <michaelward94@gmail.com>
2021-11-07 05:17:01 -0500

    update

--- doc/conf.py
+++ doc/conf.py
@@ -35,7 +35,7 @@ release = '0.0.1'
 # ones.
 extensions = ['sphinx.ext.autodoc',
 'sphinx.ext.napoleon',
-'autoapi.extension'
+'sphinx-autoapi'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
