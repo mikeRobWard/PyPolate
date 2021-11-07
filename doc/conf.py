68e7c3a446a6ffe6148ecd588ab4b056bd40c651
Michael Ward <michaelward94@gmail.com>
2021-11-07 05:12:47 -0500

    update

--- doc/conf.py
+++ doc/conf.py
@@ -34,7 +34,8 @@ release = '0.0.1'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ['sphinx.ext.autodoc',
-'sphinx.ext.napoleon'
+'sphinx.ext.napoleon',
+'autoapi.extension'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -57,3 +58,5 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+autoapi_dirs = ['../pypolate']
